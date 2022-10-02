42a5c92ec175e426752746eefad693cbbed8c1fd
fixruan <ruangoio01@gmail.com>
2022-10-02 18:29:30 -0300

    Create contact

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
--- src/screens/NewContact/index.tsx
+++ src/screens/NewContact/index.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import "react-native-get-random-values";
+import { v4 as uuidV4 } from "uuid";
 
 import { Feather } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { supabase } from "../../services/supabase";
 
 export function NewContact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+
   const navigation = useNavigation<any>();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function handleNewContact() {
+    try {
+      await supabase.post("/contacts", {
+        name,
+        email,
+        age: Number(age),
+      });
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setName("");
+      setEmail("");
+      setAge("");
+    }
   }
 
   return (
@@ -40,6 +64,8 @@ export function NewContact() {
           placeholder="Nome"
           keyboardType="default"
           keyboardAppearance="dark"
+          value={name}
+          onChangeText={setName}
         />
 
         <TextInput
@@ -47,6 +73,8 @@ export function NewContact() {
           placeholder="Email"
           keyboardType="email-address"
           keyboardAppearance="dark"
+          value={email}
+          onChangeText={setEmail}
         />
 
         <TextInput
@@ -54,9 +82,11 @@ export function NewContact() {
           placeholder="Idade"
           keyboardType="numeric"
           keyboardAppearance="dark"
+          value={age}
+          onChangeText={setAge}
         />
 
-        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={handleNewContact}>
           <Text style={styles.buttonText}>Cadastrar</Text>
         </TouchableOpacity>
       </View>
